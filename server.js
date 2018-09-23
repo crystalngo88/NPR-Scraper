3c9030f79a9a91f3cce7c01a44c0226829e3546a
Crystal Ngo <crystalngo88@gmail.com>
2018-09-23 15:24:20 -0700

    added gitignore

--- server.js
+++ server.js
@@ -1,56 +1,69 @@
+var app = express();
 var cheerio = require("cheerio");
+var collections = ["scrapedData"];
+var databaseUrl = "scraper";
+
+var express = require("require");
+var mongo = require("mongojs");
 var request = require("request");
-// var stringr = require("stringr");
-// var Nightmare = require("nightmare")
-function removeSlashes(str) {
-    // code here
-    // str.replace(/\//g, 'ForwardSlash');
-    console.log('str:', str)
-    return str.replace(/\\\//g, '');
-}
-// const { readFileSync, writeFileSync } = require('fs');
-// const numbers = readFileSync('./tesco-title-numbers.csv', 
-//   {encoding: 'utf8'}).trim().split('\n');
-// console.dir(numbers);
 
-console.log("\n********************************\n" + "Grabbing headlines from NPR.org" + "\n********************************\n");
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+    console.log("Database Error: ", error);
+});
 
-request("https://www.npr.org/", function (error, response, html) {
-    var $ = cheerio.load(html);
-    var results = [];
+app.get("/", function (req, res) {
+    res.send("Hulloooooo");
+});
 
-    $(".story-text").each(function (i, element) {
-        var title = $(element).find('.title')[0];
-        var titleText = title.children[0].data;
-
-
-
-        var link = $(element).find('a')[1];
-        var href = '';
-        if (link && link.attribs && link.attribs.href) {
-            href = link.attribs.href;
+app.get("/all", function (req, res) {
+    db.scrapedData.find({}, function (error, found) {
+        if (error) {
+            console.log(error)
+        } else {
+            res.json(found);
         }
+    });
+});
 
-        
-        var teasers = $(element).find('.teaser');
-        var teaser = teasers[0];
-        var teaserText = '';
-        if (teaser && teaser.children && teaser.children[0]) {
-            teaserText = teaser.children[0].data;
-        }
-        
-        // console.log('outside titleText', titleText)
-        // console.log('outside href', href)
-        // console.log('outside teaserText', teaserText)
+app.get("/scrape", function (req, res) {
 
-        if (titleText && href && teaserText) {
-            results.push({
-                title: titleText,
-                link: href,
-                teaser: teaserText,
-            })
-        }
+    console.log("\n********************************\n" + "Grabbing headlines from NPR.org" + "\n********************************\n");
+
+    request("https://www.npr.org/", function (error, response, html) {
+        var $ = cheerio.load(html);
+        var results = [];
+
+        $(".story-text").each(function (i, element) {
+            var title = $(element).find('.title')[0];
+            var titleText = title.children[0].data;
+
+            var link = $(element).find('a')[1];
+            var href = '';
+            if (link && link.attribs && link.attribs.href) {
+                href = link.attribs.href;
+            }
+
+            var teasers = $(element).find('.teaser');
+            var teaser = teasers[0];
+            var teaserText = '';
+            if (teaser && teaser.children && teaser.children[0]) {
+                teaserText = teaser.children[0].data;
+            }
+
+            if (titleText && href && teaserText) {
+                results.push({
+                    title: titleText,
+                    link: href,
+                    teaser: teaserText,
+                })
+            }
+
+        });
 
     });
+    app.listen(3000, function() {
+        console.log("App running on port 3000!")
+    })
     console.log(results);
 })
